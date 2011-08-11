4609328a8e7f31e64fe1e5d3230e335e0d41a9c6
Dave Simons <dave@inuits.be>
2011-08-11 12:08:39 +0200

    add params and dbconf class
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,10 @@
 import 'classes/*'
 
-class ocsinventory {
-	class{'ocsinventory::packages':} -> class{'ocsinventory::core':}
+class ocsinventory (
+	$dbuser = 'ocsinventory',
+	$dbpass = 'xoh1Dae4',
+	$dbname = 'ocsinventory'
+) {
+	class{'ocsinventory::packages':} -> class{'ocsinventory::dbconf':} -> class{'ocsinventory::core':}
 	Exec{path=>'/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin'}
 }
