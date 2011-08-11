4ed92e806d994f679819db70aacdb8f7b596e9ac
Dave Simons <dave@inuits.be>
2011-08-11 12:08:26 +0200

    add dbconf
    Signed-off-by: Dave Simons <dave@inuits.be>

---
+++ manifests/classes/dbconf.pp
@@ -0,0 +1,10 @@
+class ocsinventory::dbconf {
+	mysql_db { $ocsinventory::dbname:
+		pass => $ocsinventory::dbpass,
+		user => $ocsinventory::dbuser;
+	}
+
+	mysql_user { $ocsinventory::dbuser:
+		pass => $ocsinventory::dbpass,
+	}
+}
