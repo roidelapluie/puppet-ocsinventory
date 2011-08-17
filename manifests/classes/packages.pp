7edbd8db74d50d622efd2604371653b408379d9a
Dave Simons <dave@inuits.be>
2011-08-17 11:27:49 +0200

    comment source install stuff and add binary packages
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/packages.pp
+++ manifests/classes/packages.pp
@@ -1,15 +1,38 @@
 class ocsinventory::packages {
-	exec {
-		'source download':
-			command => 'wget http://launchpad.net/ocsinventory-server/stable-2.0/2.0/+download/OCSNG_UNIX_SERVER-2.0.tar.gz',
-			creates => '/tmp/OCSNG_UNIX_SERVER-2.0.tar.gz',
-			cwd => '/tmp';
-
-		'source unpack':
-			command => 'tar xzvf OCSNG_UNIX_SERVER-2.0.tar.gz',
-			creates => '/tmp/OCSNG_UNIX_SERVER-2.0',
-			cwd => '/tmp';
+	@package {
+		'ocsinventory':
+			ensure => present;
+		'ocsinventory-agent':
+			ensure => present;
 	}
 
-	Exec['source download'] -> Exec['source unpack']
+	realize(Package['ocsinventory-agent'])
+
+	if $ocsinventory::server != no {
+		realize(Package['ocsinventory'])
+	}
+
+	class source {
+		$pkgs=['php-mysql', 'php-gd', 'perl-XML-Simple', 'perl-Compress-Zlib', 'perl-DBI', 'perl-DBD-MySQL', 'perl-Net-IP', 'perl-XML-Entities', 'perl-Apache-DBI', 'perl-Apache2-SOAP', 'perl-SOAP-Lite', 'mod_perl']
+
+		package {
+			$pkgs:
+				ensure => present;
+		}
+
+		exec {
+			'source download':
+				command => 'wget http://launchpad.net/ocsinventory-server/stable-2.0/2.0/+download/OCSNG_UNIX_SERVER-2.0.tar.gz',
+				creates => '/tmp/OCSNG_UNIX_SERVER-2.0.tar.gz',
+				cwd => '/tmp';
+	
+			'source unpack':
+				command => 'tar xzvf OCSNG_UNIX_SERVER-2.0.tar.gz',
+				creates => '/tmp/OCSNG_UNIX_SERVER-2.0',
+				cwd => '/tmp';
+		}
+
+		Exec['source download'] -> Exec['source unpack']
+	}
+
 }
