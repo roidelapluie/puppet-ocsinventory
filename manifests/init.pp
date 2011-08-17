e98e66ce2e3979258e97058bfad49dfa15545b4d
Dave Simons <dave@inuits.be>
2011-08-17 11:24:42 +0200

    add some db params, classes
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,10 +1,13 @@
 import 'classes/*'
 
 class ocsinventory (
-	$dbuser = 'ocsinventory',
+	$dbuser = 'ocs',
 	$dbpass = 'xoh1Dae4',
-	$dbname = 'ocsinventory'
+	$dbname = 'ocs',
+	$dbhost = 'localhost',
+	$dbport = '3306',
+	$server = 'no'
 ) {
-	class{'ocsinventory::packages':} -> class{'ocsinventory::dbconf':} -> class{'ocsinventory::core':}
+	class{'ocsinventory::iptables':} -> class{'ocsinventory::repo':} -> class{'ocsinventory::packages':} -> class{'ocsinventory::dbconf':} -> class{'ocsinventory::core':} -> class{'ocsinventory::config':}
 	Exec{path=>'/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin'}
 }
