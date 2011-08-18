245986885b9b0de091403d729d2bab7c5a79b4ff
Dave Simons <dave@inuits.be>
2011-08-18 08:53:21 +0200

    comment source stuff, add install.php stuff
    Signed-off-by: Dave Simons <dave@inuits.be>

--- manifests/classes/core.pp
+++ manifests/classes/core.pp
@@ -1,18 +1,32 @@
 class ocsinventory::core {
-/*
 	file {
+/*
 		'setup.sh':
 			ensure => present,
 			path => '/tmp/OCSNG_UNIX_SERVER-2.0/setup.sh',
 			content => template('ocsinventory/setup.sh.erb'),
 			mode => 0755;
+*/
+
+		'install.php':
+			ensure => present,
+			path => '/usr/share/ocsinventory-reports/ocsreports/install.php',
+			content => template('ocsinventory/install.php.erb'),
+			mode => 0644;
 	}
 
 	exec {
+/*
 		'source install':
 			command => './setup.sh',
 			provider => shell,
 			cwd => '/tmp/OCSNG_UNIX_SERVER-2.0';
-	}
 */
+
+		'run install.php':
+			command => 'php install.php',
+			provider => shell,
+			require => File['install.php'],
+			cwd => '/usr/share/ocsinventory-reports/ocsreports';
+	}
 }
