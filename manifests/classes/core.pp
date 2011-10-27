83db73264df54717a2764412a80f77b58a51d242
Julien Pivotto <roidelapluie@inuits.eu>
2011-10-27 14:10:36 +0200

    remove confirm

--- manifests/classes/core.pp
+++ manifests/classes/core.pp
@@ -4,15 +4,9 @@ class ocsinventory::core {
     path => '/bin:/usr/bin',
     onlyif => 'test -f /usr/share/ocsinventory-reports/ocsreports/install.php',
   }
-  exec {'Install_OCS_inventory_confirm':
-    command => "wget --quiet -O /dev/null 'http://127.0.0.1/ocsreports/install.php' --post-data='host=localhost&database=ocsweb&name=${ocsinventory::dbuser}&pass=${ocsinventory::dbpass}'",
-    path => '/usr/bin',
-    onlyif => 'test -f /usr/share/ocsinventory-reports/ocsreports/install.php',
-    require => Exec['Install_OCS_inventory'];
-  }
   file {
     '/usr/share/ocsinventory-reports/ocsreports/install.php':
       ensure => absent,
-      require => Exec['Install_OCS_inventory_confirm';
+      require => Exec['Install_OCS_inventory'];
   }
 }
